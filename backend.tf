001032fd64747922d520601ae1d6ac3b5006f34c
Roberth Strand <me@robstr.dev>
2020-09-17 13:37:49 +0200

    Yes

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "demo-services-rg"
+    storage_account_name = "rsterraformdemo"
+    container_name       = "tfstate"
+    key                  = "tf"
+  }
+}
