03f510d072d15be268fcb3b28b963e16e5dc45b9
Roberth Strand <me@robstr.dev>
2020-09-17 13:43:02 +0200

    Yes

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "demo-services-rg"
     storage_account_name = "rsterraformdemo"
     container_name       = "tfstate"
-    key                  = "tf"
+    key                  = "tf.state"
   }
 }
