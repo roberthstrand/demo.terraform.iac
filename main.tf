bdcbb5be5205e1772f3c0fa96a776964c12eb5e9
Roberth Strand <me@robstr.dev>
2021-03-18 23:17:50 +0100

    Update format

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 # Define the provider as Azure
 provider "azurerm" {
-  version = "=2.20.0"
+  version = "=2.51.0"
   features {}
 }
 # Create resource group for our demo
 resource "azurerm_resource_group" "terraform" {
-    name     = "${var.deployname}-rg"
+    name     = format("%s-rg", var.deployname)
     location = var.location
 }
 
 resource "azurerm_network_security_group" "terraform" {
-  name                = "${var.deployname}-nsg"
+  name                = format("%s-nsg", var.deployname)
   location            = azurerm_resource_group.terraform.location
   resource_group_name = azurerm_resource_group.terraform.name
 }
 
 resource "azurerm_virtual_network" "terraform" {
-  name                = "${var.deployname}-vnet"
+  name                = format("%s-vnet", var.deployname)
   location            = azurerm_resource_group.terraform.location
   resource_group_name = azurerm_resource_group.terraform.name
   address_space       = ["10.0.0.0/16"]
